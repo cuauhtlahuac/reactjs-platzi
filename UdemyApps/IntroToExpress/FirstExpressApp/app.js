dadab33fcdd7ed401efa57a5f2b3d6a978da8ee9
cuauhtlahuac <cuitlagua@gmail.com>
2018-03-02 00:06:03 -0600

    test de conexion

--- UdemyApps/IntroToExpress/FirstExpressApp/app.js
+++ UdemyApps/IntroToExpress/FirstExpressApp/app.js
@@ -1,1 +1,15 @@
-console.log('Hello mortals')
+var express = require("express");
+var app = express();//se usa para que sea mas comodo el uso de express almacenar en una variable.
+
+app.get("/",function(req,res){
+	res.send("hi there");
+});
+
+
+//sin el listen express no puede correr.
+app.listen(3000,function(){
+	var conectnum = 0;
+	console.log("conected!!!")
+	conectnum + 1
+	console.log(conectnum);
+})
