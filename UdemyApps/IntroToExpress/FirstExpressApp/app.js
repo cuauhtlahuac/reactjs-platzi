df80e63ac4b9774bcaa0b3d0c05925c427003fa5
cuauhtlahuac <cuitlagua@gmail.com>
2018-03-11 23:13:51 -0600

    Nuevo ejercicio Express basic terminado

--- UdemyApps/IntroToExpress/FirstExpressApp/app.js
+++ UdemyApps/IntroToExpress/FirstExpressApp/app.js
@@ -13,8 +13,27 @@ app.get("/dog",function(req,res){
 	res.send("Miau!");
 	console.log("Se hizo una solicitud");
 });
+// el sig requerimiento imprime el texto despues de cumplir el patron...
+app.get("/r/:name?/comments/:id/:post",function(req,res){
+	console.log(req.params);
+	res.send("comments section...");
+});
+
+//para saber el valor que se puso de algun patron
+app.get("/r/:subname",function(req,res){
+	var subN = req.params.subname;
+	res.send("Bienvenido a el " + subN + " Show...");
+}); 
+
+app.get("*",function(req,res){
+	res.send("You are a star ");
+});
+
+setTimeout(function() {
+	console.log("1 second less")
+}, 1000);
 
 //sin el listen express no puede correr.
 app.listen(3000,function(){
 	console.log("conected!!!");
-})
+});
