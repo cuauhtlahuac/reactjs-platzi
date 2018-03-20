0c50aa0ff29317c3e7544ccddc9ba0e1f2a44818
cuauhtlahuac <cuitlagua@gmail.com>
2018-03-19 21:19:13 -0600

    Buscador de peliculas terminado

--- UdemyApps/IntroToExpress/moreExpress/movie_search_app/app.js
+++ UdemyApps/IntroToExpress/moreExpress/movie_search_app/app.js
@@ -1,15 +1,29 @@
 var express = require('express');
 var app = express();
 var request = require("request");
+var bodyParser = require('body-parser');
+app.set("view engine", "ejs");
+
+app.get("/",function(req,res){
+    res.render("searcher")
+})
 
 app.get("/result",function(req,res){
-    request("http://www.omdbapi.com/?s=garfield&y=2004&apikey=thewdb&callback",function(error,response,body){
+    var newMovie = req.query.movies;
+    console.log(`New search of the title: - ${newMovie} -`)
+    request(`http://www.omdbapi.com/?s=${newMovie}&page=1&apikey=thewdb`,function(error,response,body){
         if(!error && response.statusCode == 200){
-            var result = JSON.parse(body);
-            res.send(result["Search"][0]);
-        }
+            var data = JSON.parse(body);
+            res.render("result",{data: data , name: newMovie})
+        }     
     })
 })
+
+/*app.post("/newSearch",function(req,res){
+    var newMovie = req.body;
+    //res.redirect("/result");
+    res.send("Movie: " + newMovie)
+});*/
 
 app.listen(3000, "localhost",function(){
     console.log("Movie App conected !!!")
